6e96d0c7fd2124371741a1f23a1a4059069defc2
unknown <jcmm1519@gmail.com>
2023-02-01 23:52:29 -0500

    first-commit-curso-py

--- PracticPaython/ejercicios-1/ejercicio-1.2.py
+++ PracticPaython/ejercicios-1/ejercicio-1.2.py
@@ -2,6 +2,6 @@ frase = input("Decime una frase y te calculo cuanto tardarias si tuvieras que de
 cantidad_de_palabras = frase.split(" ")
 cantidad_de_palabras =len(cantidad_de_palabras)
 if cantidad_de_palabras >12:
-    print("para flaco tampoco le pedi un testamento")
+    print("no le pedi un testamento")
 print(f'Digiste {cantidad_de_palabras} palabras, y tardaria {cantidad_de_palabras/2}segundos en decirlo')
 print(f'rancio lo dice {cantidad_de_palabras * 100 // 2*1.3 /100} segundos')
